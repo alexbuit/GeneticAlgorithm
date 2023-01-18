aacbc477e26c26442320d287c2a926402336e875
vanei <vaneikadrian@gmail.com>
2023-01-18 13:37:16 +0100

    Added to the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,9 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot',
                 'matplotlib.mpl_toolkits']
+
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
