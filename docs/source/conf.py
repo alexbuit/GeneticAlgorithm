51c115df90518c345fa427721e1353a534344622
vanei <vaneikadrian@gmail.com>
2023-01-18 16:08:49 +0100

    Added to the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 import mock
 
 MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot',
-                'mpl_toolkits', 'scipy', 'scipy.optimize']
+                'mpl_toolkits.axes_grid1', 'scipy', 'scipy.optimize']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
