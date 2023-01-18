73656eb6b6620dbaea835564cc7c419c1d9867a8
vanei <vaneikadrian@gmail.com>
2023-01-18 13:32:17 +0100

    Added to the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
+                'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
