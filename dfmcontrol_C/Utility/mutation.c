c2bfaa6279d95d433a8ffa42dcbf819472a428d5
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2023-12-09 21:18:55 +0100

    Added the mutations to C

--- dfmcontrol_C/Utility/mutation.c
+++ dfmcontrol_C/Utility/mutation.c
@@ -2,3 +2,33 @@
 #include "stdlib.h"
 #include "math.h"
 
+void mutate(int *bit, int size, float mutate_coeff_rate){
+
+    // mutate_coeff_rate is the amount of mutations over the bit;
+    // check if mutate_coeff_rate is < size
+
+    if (mutate_coeff_rate > size){
+        printf("Error: mutate_coeff_rate is bigger than size\n");
+        exit(1);
+    }
+
+    int *mutations = malloc(size * sizeof(int));
+    // generate random mutations, that are not at the same position
+    for (int i = 0; i < size; i++){
+        mutations[i] = rand() % size;
+        for (int j = 0; j < i; j++){
+            if (mutations[i] == mutations[j]){
+                i--;
+                break;
+            }
+        }
+    }
+    
+    // mutate the bit
+    for (int i = 0; i < mutate_coeff_rate; i++){
+        bit[mutations[i]] = !bit[mutations[i]];
+    }
+
+    free(mutations);
+
+}
