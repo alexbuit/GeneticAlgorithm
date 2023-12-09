178e2818012d9df43d938b4b14949158423374bf
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2023-12-09 21:57:14 +0100

    Updated the readthedocs conf file to allow mock to be used.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
-              'sphinx.ext.autosectionlabel','sphinx.ext.intersphinx', 'sphinx_rtd_theme']
+              'sphinx.ext.autosectionlabel','sphinx.ext.intersphinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -38,5 +38,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
