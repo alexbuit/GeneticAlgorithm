f4a473033f6bf220fb34dba81e0a97917a0a9254
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2023-12-09 21:41:49 +0100

    Updated the readthedocs conf file to allow mock to be used.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,14 +8,13 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.abspath('sphinxext'))
 
-import mock
 
-MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot',
+autodoc_mock_imports = ['numpy', 'matplotlib', 'matplotlib.pyplot',
                 'mpl_toolkits.axes_grid1', 'scipy', 'scipy.optimize',
                 'matplotlib.animation', 'numpy.random', 'scipy.stats']
 
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+# for mod_name in MOCK_MODULES:
+#     sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
