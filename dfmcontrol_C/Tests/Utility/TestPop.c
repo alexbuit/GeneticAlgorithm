e2e9c22e97f1e4ce88ef7eaca26ebccf00dd8b15
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2023-12-12 23:41:26 +0100

    Added tests for population initialsation with the option to write the resulting pop to a file so it can be checked for the distribution in python

--- dfmcontrol_C/Tests/Utility/TestPop.c
+++ dfmcontrol_C/Tests/Utility/TestPop.c
@@ -9,6 +9,7 @@
 
 int main(){
     Testbitpop(32, 16, 16, 1);
+    Testuniformpop(32, 16, 16, 10.0f, 1.0f, 1, 1);
 }
 
 void Testbitpop(int bitsize, int genes, int individuals, int writeresult){
@@ -35,46 +36,35 @@ void Testbitpop(int bitsize, int genes, int individuals, int writeresult){
         // Write result to file
         FILE* fp;
         fp = fopen("TestBitPop.txt", "w");
-        fprintf(fp, "cols: %d\n", genes * bitsize);
-        fprintf(fp, "rows: %d\n", individuals);
-
-        fprintf(fp, "[");
+        fprintf(fp, "Binarystring\n");
         for (int i = 0; i < individuals; i++) {
-            fprintf(fp, "[");
+            fprintf(fp, "");
             for (int j = 0; j < genes * bitsize; j++) {
                 fprintf(fp, "%d", result[i][j]);
-                if (j < genes * bitsize - 1) {
-                    fprintf(fp, ", ");
-                }
             }
-            fprintf(fp, "]");
-            if (i < individuals - 1) {
-                fprintf(fp, ", \n");
-            }
+            fprintf(fp, "\n");
         }
-        fprintf(fp, "]\n");
+        fprintf(fp, "\n");
 
         // print numerical values
-        fprintf(fp, "[");
+        fprintf(fp, "inidviduals %d genes %d bitsize %d \n", individuals, genes, bitsize);
+        fprintf(fp, "x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15\n");
 
         for (int i = 0; i < individuals; i++) {
-            fprintf(fp, "[");
             for (int j = 0; j < genes; j++) {
                 fprintf(fp, "%.4f", numresult[i][j]);
                 if (j < genes - 1) {
-                    fprintf(fp, ", ");
+                    fprintf(fp, ",");
                 }
             }
-            fprintf(fp, "]");
-            if (i < individuals - 1) {
-                fprintf(fp, ", \n");
-            }
+            fprintf(fp, "\n");
         }
 
-        fprintf(fp, "]\n");
+        fprintf(fp, "\n");
 
         fclose(fp);
     }   
+
 
 
 
@@ -89,8 +79,69 @@ void Testbitpop(int bitsize, int genes, int individuals, int writeresult){
     
 }
 
-void Testuniformpop(int bitsize, int genes, int individuals, int writeresult){
+void Testuniformpop(int bitsize, int genes, int individuals,
+                     float factor, float bias, int normalised, int writeresult){
 
+        int** result = (int**)malloc(individuals * sizeof(int*));
+    for (int i = 0; i < individuals; i++){
+        result[i] = (int*)malloc(genes * bitsize * sizeof(int));
+    }
+
+    float** numresult = malloc(individuals * sizeof(float*));
+
+    for (int i = 0; i < individuals; i++){
+        numresult[i] = malloc(genes * sizeof(float));
+    }
+
+    bitpop(bitsize, genes, individuals, result);
+
+    ndbit2int(result, bitsize, genes, individuals, factor, bias, normalised, numresult);
+
+    printMatrix(result, individuals, genes * bitsize);
+    printfMatrix(numresult, individuals, genes);
+
+    if (writeresult == 1){
+        // Write result to file
+        FILE* fp;
+        fp = fopen("TestBitUniformPop.txt", "w");
+        fprintf(fp, "Binarystring\n");
+        for (int i = 0; i < individuals; i++) {
+            fprintf(fp, "");
+            for (int j = 0; j < genes * bitsize; j++) {
+                fprintf(fp, "%d", result[i][j]);
+            }
+            fprintf(fp, "\n");
+        }
+        fprintf(fp, "\n");
+
+        // print numerical values
+        fprintf(fp, "inidviduals %d genes %d bitsize %d factor %.4f bias %.4f normalised %d\n", individuals, genes, bitsize, factor, bias, normalised);
+        fprintf(fp, "x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15\n");
+
+        for (int i = 0; i < individuals; i++) {
+            for (int j = 0; j < genes; j++) {
+                fprintf(fp, "%.4f", numresult[i][j]);
+                if (j < genes - 1) {
+                    fprintf(fp, ",");
+                }
+            }
+            fprintf(fp, "\n");
+        }
+
+        fprintf(fp, "\n");
+
+        fclose(fp);
+    }   
+
+
+
+    for (int i = 0; i < individuals; i++){
+        free(numresult[i]);
+        free(result[i]);
+        
+    }
     
+    free(numresult);
+    free(result);
 
 }
